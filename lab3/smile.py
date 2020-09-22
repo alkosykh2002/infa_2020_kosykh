bfeb97afee36c5f93089f0e3134bc37522b77916
alkosykh2002 <kosykh.ae@phystech.edu>
2020-09-22 23:43:54 +0300

    smile.py is work now

--- lab3/smile.py
+++ lab3/smile.py
@@ -8,15 +8,17 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((1200, 720))
+screen.fill((255, 255, 255))
 
-#rect(screen, (255, 0, 255), (100, 100, 200, 200))
-#rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-#polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               #(300,100), (100,100)])
-#polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                             #  (300,100), (100,100)], 5)
-circle(screen, (255, 255, 0), (200, 200), 200)
-circle(screen, (255, 255, 255), (100, 75), 25, 5)
+circle(screen, (255, 255, 0), (600, 400), 200)
+circle(screen, (255, 0, 0), (500, 350), 40)
+circle(screen, (255, 0, 0), (700, 350), 30)
+circle(screen, (0, 0, 0), (500, 350), 20)
+circle(screen, (0, 0, 0), (700, 350), 15)
+line(screen, (0, 0, 0), (500, 500), (700, 500), 50)
+polygon(screen, (0, 0, 0), [[550,360], [560, 350], [450, 240], [440, 250]])
+polygon(screen, (0, 0, 0), [[650, 350], [644, 340], [764, 270], [770, 280]])
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
