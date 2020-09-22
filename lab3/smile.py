3a95667b3f0d581ecee1177325a9222de4ccd450
alkosykh2002 <kosykh.ae@phystech.edu>
2020-09-22 16:44:43 +0300

    added file smile.py

---
+++ lab3/smile.py
@@ -0,0 +1,31 @@
+print('z ujdyjtl')
+
+
+import pygame
+from pygame.draw import *
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((1200, 720))
+
+#rect(screen, (255, 0, 255), (100, 100, 200, 200))
+#rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
+#polygon(screen, (255, 255, 0), [(100,100), (200,50),
+                               #(300,100), (100,100)])
+#polygon(screen, (0, 0, 255), [(100,100), (200,50),
+                             #  (300,100), (100,100)], 5)
+circle(screen, (255, 255, 0), (200, 200), 200)
+circle(screen, (255, 255, 255), (100, 75), 25, 5)
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
