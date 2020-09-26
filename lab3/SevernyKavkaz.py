0723ec3e88fb7dd41d3cb7c88091d1fa986eb419
alkosykh2002 <kosykh.ae@phystech.edu>
2020-09-27 02:42:37 +0300

    sdelal more i korablik

--- lab3/SevernyKavkaz.py
+++ lab3/SevernyKavkaz.py
@@ -39,7 +39,7 @@ def gornychrebet1():
         if (x > 1100) and (x < 1197):
             y = y + (x - 1200) / 200
         if x > 1197:
-            y = y + 253
+            y = y + 150
 
     print(x, y)
     polygon(screen, [139, 109, 92], Gor)
@@ -55,13 +55,26 @@ def solntse(x, y, r):
 
 
 def more(x, y, t):
-    volna = [[0, 0]] * 120
+    volna = [[0, 0]] * 130
     for i in range(0, 110):
-        for j in range(0, 120):
+        for j in range(0, 130):
             volna[j] = [x + 10 * j, y + i * 3 + 3 * (j % 3)]
         pygame.draw.lines(screen,
                           [120 * (i + t) % 3, 120 + 30 * ((t + i) % 3) * ((i + t) % 3), 170 + 5 * ((i + t) % 3)],
                           0, volna, 3)
+
+
+def korablik(x, y, r):
+    pygame.draw.polygon(screen, (184, 16, 0),
+                        [[x, y + 7 * r / 10], [x + 3 * r / 10, y + r], [x + r, y + r], [x + r, y + 8 * r / 10]])
+    pygame.draw.polygon(screen, [255, 0, 0], [[x + 0.6 * r, y + 0.2 * r], [x + 0.6 * r, y], [x + 0.9 * r, y + 0.1 * r]])
+    pygame.draw.polygon(screen, [255, 255, 255],
+                        [[x + 0.6 * r, y + 0.76 * r], [x + 0.6 * r, y + 0.2 * r], [x, y + 0.7 * r],
+                         [x + 0.3 * r, y + 0.65 * r]])
+    pygame.draw.polygon(screen, [255, 255, 255],
+                        [[x + 0.6 * r, y + 0.76 * r], [x + 0.6 * r, y + 0.2 * r], [x + r, y + 0.8 * r],
+                         [x + r, y + 0.7 * r]])
+    pygame.draw.line(screen, [184, 16, 0], [x + 0.6 * r, y], [x + 0.6 * r, y + 0.8 * r], round(r/20))
 
 
 # main programm
@@ -77,7 +90,9 @@ time = 0
 while not finished:
     clock.tick(FPS)
     more(0, 400, round(time))
-    time = time + 10
+    korablik(1000 + round(2 * time), 600, 50)
+    time = time - 0.1
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
