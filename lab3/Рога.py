4f6d18b5f93b367d4cd79c5f65b5b584f8e6d8c5
alkosykh2002 <kosykh.ae@phystech.edu>
2020-10-14 17:11:25 +0300

    Принял правки Костяна

--- lab3/Рога.py
+++ lab3/Рога.py
@@ -22,33 +22,44 @@ exp_x1 = 0
 exp_y1 = 0
 Ecrx = 1000
 Ecry = 700
-FPS = 20
-cursor_pos = 0
+FPS = 60
+cursor_pos = [0, 0]
 screen = pygame.display.set_mode((Ecrx, Ecry))
+okno_x_min = 100
+okno_x_max = 900
+okno_y_min = 100
+okno_y_max = 600
 
 pygame.font.SysFont('arial', 36)
 f1 = pygame.font.Font(None, 30)
 
 
 class Hero:
-    x = 0
-    y = 0
-    fi = 0
-    r = 50
-    tsvet_tela = COLORS[0]
-    tsvet_planseta = COLORS[3]
+    def __init__(self):
+        self.x = 200
+        self.y = 300
+        self.fi = 0
+        self.r = 50
+        self.tsvet_tela = COLORS[0]
+        self.tsvet_planseta = COLORS[3]
+        self.gun_x = int(self.x + 2 * self.r * numpy.cos(self.fi))
+        self.gun_y = int(self.y + 2 * self.r * numpy.sin(self.fi))
 
     def vverh(self):
-        self.y -= 100 / FPS
+        if self.y > okno_y_min + self.r:
+            self.y -= 100 / FPS
 
     def vniz(self):
-        self.y += 100 / FPS
+        if self.y < okno_y_max - self.r:
+            self.y += 100 / FPS
 
     def vlevo(self):
-        self.x -= 100 / FPS
+        if self.x > okno_x_min + self.r:
+            self.x -= 100 / FPS
 
     def vpravo(self):
-        self.x += 100 / FPS
+        if self.x < okno_x_max - self.r:
+            self.x += 100 / FPS
 
     def new_coord(self, dx, dy):
         return [self.x + dx * self.r * numpy.cos(self.fi) + dy * self.r * numpy.sin(self.fi),
@@ -77,8 +88,8 @@ class Hero:
         circle(screen, [255, 255, 150], [int(self.x), int(self.y)], int(0.2 * self.r))
 
     def vystrel(self):
-        line(screen, [255, 200, 100], [int(self.x), int(self.y)],
-             [int(self.x + 1000 * numpy.cos(self.fi)), int(self.y + 1000 * numpy.sin(self.fi))])
+        bullet = Bullets()
+        return bullet
 
 
 def explosion(x, y, t):
@@ -93,83 +104,106 @@ def explosion(x, y, t):
 
 
 class SharOdin:
-    parametry = [0, 0, 0, 0, 0, 0]
     time_of_birthday = 0
 
     def __init__(self):
-        x = randint(200, Ecrx - 200)
-        y = randint(200, Ecry - 200)
-        r = randint(40, 100)
-        speed_x = randint(-100, 100) / FPS
-        speed_y = randint(-100, 100) / FPS
+        self.x = randint(200, Ecrx - 200)
+        self.y = randint(200, Ecry - 200)
+        self.r = randint(40, 100)
+        self.speed_x = randint(-100, 100) / FPS
+        self.speed_y = randint(-100, 100) / FPS
         self.time_of_birthday = time
-        self.parametry = [x, y, r, speed_x, speed_y, time]
 
     def dvizh(self):
-        self.parametry[5] = time
-        if self.parametry[5] - self.time_of_birthday < 200:
-            self.parametry[0] += self.parametry[3]
-            self.parametry[1] += self.parametry[4]
-            if self.parametry[0] < 100 + self.parametry[2]:
-                self.parametry[3] = -self.parametry[3]
-                self.parametry[0] = self.parametry[0] + 2 * int(round(self.parametry[3]))
-                self.parametry[4] = randint(-100, 100) / FPS
-            if self.parametry[0] > Ecrx - 100 - self.parametry[2]:
-                self.parametry[3] = -self.parametry[3]
-                self.parametry[0] = self.parametry[0] + 2 * int(round(self.parametry[3]))
-                self.parametry[4] = randint(-100, 100) / FPS
-            if self.parametry[1] < 100 + self.parametry[2]:
-                self.parametry[4] = -self.parametry[4]
-                self.parametry[1] = self.parametry[1] + 2 * int(round(self.parametry[4]))
-                self.parametry[3] = randint(-100, 100) / FPS
-            if self.parametry[1] > Ecry - 100 - self.parametry[2]:
-                self.parametry[4] = -self.parametry[4]
-                self.parametry[1] = self.parametry[1] + 2 * int(round(self.parametry[4]))
-                self.parametry[3] = randint(-100, 100) / FPS
+        if self.time_of_birthday - self.time_of_birthday < 200:
+            self.x += self.speed_x
+            self.y += self.speed_y
+            if self.x < okno_x_min + self.r:
+                self.speed_x = -self.speed_x
+                self.x = self.x + 2 * int(round(self.speed_x))
+                self.speed_y = randint(-100, 100) / FPS
+            if self.x > okno_x_max - self.r:
+                self.speed_x = -self.speed_x
+                self.x = self.x + 2 * int(round(self.speed_x))
+                self.speed_y = randint(-100, 100) / FPS
+            if self.y < okno_y_min + self.r:
+                self.speed_y = -self.speed_y
+                self.y = self.y + 2 * int(round(self.speed_y))
+                self.speed_x = randint(-100, 100) / FPS
+            if self.y > okno_y_max - self.r:
+                self.speed_y = -self.speed_y
+                self.y = self.y + 2 * int(round(self.speed_y))
+                self.speed_x = randint(-100, 100) / FPS
 
     def risyi(self):
-        if self.parametry[5] - self.time_of_birthday < 150:
+        if time - self.time_of_birthday < 150:
             circle(screen, [50 + (time - self.time_of_birthday) % 150, 0, 0],
-                   [int(self.parametry[0]), int(self.parametry[1])], self.parametry[2])
-            circle(screen, [0, 0, 0], [int(self.parametry[0]), int(self.parametry[1])],
-                   self.parametry[2] + 3, 3)
-        if (self.parametry[5] - self.time_of_birthday > 150) and (
-                self.parametry[5] - self.time_of_birthday < 201):
-            circle(screen, [255, 255, 0], [int(self.parametry[0]), int(self.parametry[1])],
-                   self.parametry[2])
-            circle(screen, [0, 0, 0], [int(self.parametry[0]), int(self.parametry[1])],
-                   self.parametry[2] + 3, 3)
-        if self.parametry[5] - self.time_of_birthday > 200:
-            explosion(int(self.parametry[0]), int(self.parametry[1]),
-                      self.parametry[5] - self.time_of_birthday - 200)
+                   [int(self.x), int(self.y)], self.r)
+            circle(screen, [0, 0, 0], [int(self.x), int(self.y)],
+                   self.r + 3, 3)
+        if (time - self.time_of_birthday > 150) and (
+                time - self.time_of_birthday < 201):
+            circle(screen, [255, 255, 0], [int(self.x), int(self.y)],
+                   self.r)
+            circle(screen, [0, 0, 0], [int(self.x), int(self.y)],
+                   self.r + 3, 3)
+        if time - self.time_of_birthday > 200:
+            explosion(int(self.x), int(self.y),
+                      time - self.time_of_birthday - 200)
 
-    def check(self, hero, cursor):
-        if abs((cursor[1] - hero[1]) * self.parametry[0] + (hero[0] - cursor[0]) * self.parametry[1] - hero[0] * (
-                cursor[1] - hero[1]) + hero[1] * (cursor[0] - hero[0])) / numpy.sqrt(
-            (cursor[1] - hero[1]) * (cursor[1] - hero[1]) + (cursor[0] - hero[0]) * (cursor[0] - hero[0])) < \
-                self.parametry[2]:
+    def check(self, x, y, r):
+        if (self.x - x) ** 2 + (self.y - y) ** 2 <= (self.r + r) ** 2:
             return 1
         else:
             return 0
+
+
+class Bullets:
+    def __init__(self):
+        self.x = player.x
+        self.y = player.y
+        self.r = 5
+        self.speed = 300 / FPS
+        self.fi = player.fi
+        self.live = True
+        self.color = (255, 0, 0)
+
+    def dvizh(self):
+        if (self.x > okno_x_min) and (self.x < okno_x_max) and self.live:
+            if (self.y > okno_y_min) and (self.y < okno_y_max):
+                self.y += self.speed * numpy.sin(self.fi)
+            else:
+                self.live = False
+            self.x += self.speed * numpy.cos(self.fi)
+        else:
+            self.live = False
+
+    def risyi(self):
+        circle(screen, self.color, (int(self.x), int(self.y)), self.r)
 
 
 clock = pygame.time.Clock()
 finished = False
 
 shar_1 = SharOdin()
-print(shar_1.parametry)
 shar_2 = SharOdin()
 shar_3 = SharOdin()
 shar_4 = SharOdin()
 shar_5 = SharOdin()
 player = Hero()
-
+bullet = Bullets()
+bullet.x = 0
+bullet.y = 0
 while not finished:
     clock.tick(FPS)
     screen.fill([0, 0, 0])
-    polygon(screen, [255, 255, 255], [[100, 100], [100, 600], [900, 600], [900, 100]], 5)
-    time += 1
+    polygon(screen, [255, 255, 255],
+            [[okno_x_min - 5, okno_y_min - 5], [okno_x_min - 5, okno_y_max + 5], [okno_x_max + 5, okno_y_max + 5],
+             [okno_x_max + 5, okno_y_min - 5]], 5)
+    time += 0.02 * FPS
     screen.blit(f1.render('score = ' + str(score), 1, (255, 255, 255)), (0, 0))
+    bullet.dvizh()
+    bullet.risyi()
     shar_1.dvizh()
     shar_1.risyi()
     keys = pygame.key.get_pressed()
@@ -182,6 +216,9 @@ while not finished:
         player.vniz()
     if keys[pygame.K_d]:
         player.vpravo()
+    if shar_1.check(bullet.x, bullet.y, bullet.r):
+        score += 1
+        shar_1 = SharOdin()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -190,10 +227,7 @@ while not finished:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if (event.button == 1) or (event.button == 3):
                 cursor_pos = event.pos
-                player.vystrel()
-                if shar_1.check([player.x, player.y], cursor_pos):
-                    score += 1
-                    shar_1 = SharOdin()
+                bullet = player.vystrel()
     player.risyi(cursor_pos)
     pygame.display.update()
 pygame.quit()
